eb54ad377ded4243a385636779b7cae86b22575f
nmakdessi <21nmakdessi@athenian.org>
2020-09-15 05:29:11 +0000

    Completed the code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int last = binary.length()-1;
+        int decimal = 0;
+        for(int i = 0; i < binary.length(); i++ ){
+            decimal += (Integer.parseInt(binary.charAt(last) + "")) * Math.pow(2, i);
+            last--;
+        }
+        return decimal;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number in binary");
+        String numb = scan.nextLine();
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(numb));
+        scan.close();
     }
+    
 }
